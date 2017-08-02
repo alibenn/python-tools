8a5b8dde8afeca22aac99d6e36c0dc3782fa37b2
Albert Bursche <Albert Bursche bursche@cern.ch>
2017-08-02 11:37:57 +0200

    fixed difference in date format in klist on SCL6 and centOS7 Could be done more pythonic

--- credentials/copyKRB.py
+++ credentials/copyKRB.py
@@ -1,24 +1,24 @@
-osslcmd = "openssl x509 -in /tmp/x509up_u`id -u` -noout -enddate"
 import subprocess
 def getKerberosDate(hostname):
     osslcmd = "ssh %s klist"%hostname
     proc = subprocess.Popen(osslcmd,stdout=subprocess.PIPE,shell=True)
     (out, err) = proc.communicate()
-    outwithoutreturn = out.rstrip('\n')[9:]    
-    pos = outwithoutreturn.find("renew until")
-    if pos>0:
-        return outwithoutreturn[pos+12:pos+31]
+    for l in out.split("\n"):
+        l = l.replace("201","1") ## 2017 -> 17 ... 
+        if "afs/cern.ch@CERN.CH" in l:
+            return l[19:36]
+#    outwithoutreturn = out.rstrip('\n')[9:]    
+#    print out ## debug
+#    pos = outwithoutreturn.find("renew until")
+#    if pos>0:
+#        return outwithoutreturn[pos+12:pos+31]
 
 
 import datetime
 def ConvertDate(inp):
-    return datetime.datetime.strptime(inp,"%m/%d/%Y %X")
+    return datetime.datetime.strptime(inp,"%m/%d/%y %X")
 
-
-servers = ["lhcbportal",
-           "lhcbwn01",
-           "lhcbwn02",
-           "lhcbwn03"]
+from servers import servers
 
 def getBest():
     ts = {}
@@ -36,6 +36,17 @@ def copy():
         proc = subprocess.Popen("scp /tmp/krb5cc_`id -u` %s:/tmp/krb5cc_`id -u`"%s,shell=True)
         (out, err) = proc.communicate()
 
+
+def renew():
+    for s in servers:
+        proc = subprocess.Popen("ssh %s kinit -R"%s,shell=True)
+        (out, err) = proc.communicate()
+
+def aklog():
+    for s in servers:
+        proc = subprocess.Popen("ssh %s aklog"%s,shell=True)
+        (out, err) = proc.communicate()
+
 import sys
 
 argv = sys.argv
@@ -47,12 +58,14 @@ def show():
         print s,getKerberosDate(s)
 
 def usage():
-    print "python copy.py [show|copy|getbest|usage]"
+    print "python copy.py [show|copy|getbest|usage|renew|aklog]"
 
 TheDict = {"show":show,
            "getbest":getBest,
            "copy":copy,
-           "usage":usage}
+           "usage":usage,
+           "renew":renew,
+           "aklog":aklog}
 
 for cmd in argv[1:]:
     TheDict[cmd]()
